8aa2d7d1d14d5373ffa90ecd95b4e1e0cb973aff
Lukasz Buchmiet <56140308+buchmiet@users.noreply.github.com>
2025-07-28 19:08:44 +0100

    morn generation added 2

---
+++ Generator/ModernGeneration/Policies/IAsyncPolicy.cs
@@ -0,0 +1,26 @@
+namespace FastStateMachine.Generator.ModernGeneration.Policies;
+
+public interface IAsyncPolicy
+{
+    // Transformacje typów zwracanych
+    string ReturnType(string syncType);
+    
+    // Słowa kluczowe
+    string AsyncKeyword();
+    string AwaitKeyword(bool targetIsAsync);
+    string ConfigureAwait();
+    
+    // Parametry i argumenty
+    string CancellationTokenParam();
+    string CancellationTokenArg();
+    
+    // Nazwy metod
+    string MethodName(string baseName, bool addAsyncSuffix = true);
+    
+    // Helpery do emisji kodu
+    void EmitInvocation(System.Text.StringBuilder sb, string methodName, bool methodIsAsync, params string[]? args);
+    
+    // Właściwości
+    bool IsAsync { get; }
+    bool UseSyncAdapter { get; }
+}
