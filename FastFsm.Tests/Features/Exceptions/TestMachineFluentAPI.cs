68f3dd33ffca1049421f02d6c855dc39bb03aa39
Lukasz <plant@o2.pl>
2025-09-01 14:40:10 +0100

    chore(release): v0.0.0.7-fluentapiv2

---
+++ FastFsm.Tests/Features/Exceptions/TestMachineFluentAPI.cs
@@ -0,0 +1,17 @@
+using Abstractions.Attributes;
+using Abstractions.Fluent;
+
+namespace FastFsm.Tests.Features.Exceptions
+{
+    /// <summary>
+    /// Fluent API version of TestMachine
+    /// </summary>
+    [StateMachine(typeof(State), typeof(Trigger), GenerateExtensibleVersion = true)]
+    public partial class TestMachineFluentAPI
+    {
+        private static void Configure() => FSM
+            .State(State.Initial)
+                .On(Trigger.Next).GoTo(State.Final)
+            .State(State.Final);
+    }
+}
