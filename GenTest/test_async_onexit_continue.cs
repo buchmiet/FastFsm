aa1373b8c84b4840d682de39fa13107f81e77267
Lukasz Buchmiet <plant@o2.pl>
2025-09-07 00:15:44 +0100

    chore(release): v0.0.0.20-feature-fluent-hsm-parser-v0.7.5

---
+++ GenTest/test_async_onexit_continue.cs
@@ -0,0 +1,30 @@
+[StateMachine(typeof(S), typeof(T))]
+[OnException(nameof(HandleAsync))]
+public partial class AsyncOnExitContinueMachine
+{
+    public enum S { Idle, Running }
+    public enum T { Start }
+
+    [State(S.Idle, OnExit = nameof(OnExitIdleAsync))]
+    [Transition(S.Idle, T.Start, S.Running)]
+    private void Configure() { }
+
+    public static bool ThrowOnExit { get; set; } = true;
+
+    private async System.Threading.Tasks.Task OnExitIdleAsync()
+    {
+        await System.Threading.Tasks.Task.Yield();
+        if (ThrowOnExit) throw new System.InvalidOperationException("boom");
+    }
+
+    private async System.Threading.Tasks.ValueTask<FastFsm.Exceptions.ExceptionDirective> HandleAsync(
+        FastFsm.Exceptions.ExceptionContext<S, T> ctx,
+        System.Threading.CancellationToken ct)
+    {
+        await System.Threading.Tasks.Task.Yield();
+        return FastFsm.Exceptions.ExceptionDirective.Continue;
+    }
+}
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Generator")]
+using Abstractions.Attributes;
+using FastFsm.Exceptions;
